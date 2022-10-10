e4f5344c935c6db0fa7d7372360f5a3a60a21fb5
Alexandros Fytilis <alexandrosfytilis@hotmail.com>
2022-10-09 23:04:41 -0400

    Added vegetables to vegetableData

--- client/src/general/utils/vegatableData.js
+++ client/src/general/utils/vegatableData.js
@@ -1,6 +1,125 @@
 export const vegetableData = [
   {
+    name: "Acorn Squash",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Anaheim Pepper",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Artichoke",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Arugula Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Asparagus",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Buttter Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Butternit Squash",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Eggplant",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Avocado",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Beet",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Bok Choy",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Broccoli",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Broccolini",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Broccoli Raab",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Brussel Sprout",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
     name: "Carrot",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Cauliflower",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Celeriac",
     description: "This is a Carrot",
     img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
     harvest: 60,
@@ -14,9 +133,51 @@ export const vegetableData = [
     growth: 30
   },
   {
+    name: "Chard",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Cherry Tomato",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Chicory",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Chinese Cabbage",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Collard Greens",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
     name: "Corn",
     description: "This is a Corn",
     img: "https://images.pexels.com/photos/547263/pexels-photo-547263.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Cress",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
     harvest: 60,
     growth: 30
   },
@@ -28,9 +189,156 @@ export const vegetableData = [
     growth: 0
   },
   {
+    name: "Daikon",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Endive Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Garlic",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Green Leaf Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Green Beans",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Green Cabbage",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Greens",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Green Onion",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Gourde",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Jicama",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Kale",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Kohlrabi",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Leeks",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Lima Beans",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Mushroom",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Mustard Greens",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Okra",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
     name: "Onion",
     description: "This is a Onion",
     img: "https://media.istockphoto.com/photos/onion-bulbs-picture-id513920379?b=1&k=20&m=513920379&s=612x612&w=0&h=1GLAk73uklpdUl-D9--jqYPtuYStgmgM-YOImwONulo=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Patti Pan Squash",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Parsnip",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Peas",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
     harvest: 60,
     growth: 30
   },
@@ -42,6 +350,27 @@ export const vegetableData = [
     growth: 30
   },
   {
+    name: "Pumpkin",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Purple Cabbage",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Radicchio",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
     name: "Raddish",
     description: "This is a Raddish",
     img: "https://media.istockphoto.com/photos/small-garden-radish-picture-id121137414?b=1&k=20&m=121137414&s=612x612&w=0&h=BRgI7t4aWcfki3rFMngQkn0kbJfEtF7UiQPPR0sL-lQ=",
@@ -49,9 +378,135 @@ export const vegetableData = [
     growth: 0
   },
   {
+    name: "Red Leaf Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Rhubarb",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Romaine Lettuce",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Romanesco",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Rutabaga",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Savoy Cabbage",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Shallots",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Spaghetti Squash",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Spinach",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Squash",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Swede",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Sweet Pepper",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Sweet Potato",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Tomatillo",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
     name: "Tomato",
     description: "This is a Tomato",
     img: "https://media.istockphoto.com/photos/tomato-isolated-tomato-on-white-background-with-clipping-path-full-picture-id941825808?b=1&k=20&m=941825808&s=612x612&w=0&h=vqOse4sHymYRm5a3JZaZ9W6tMeSJIM5qkK0l4qus014=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Turnip",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Water Chestnuts",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Yam",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
+    harvest: 60,
+    growth: 30
+  },
+  {
+    name: "Zucchini",
+    description: "This is a Carrot",
+    img: "https://media.istockphoto.com/photos/carrots-picture-id92026520?b=1&k=20&m=92026520&s=612x612&w=0&h=ffjqUSiKMk0d314KQdHsX77b9-7UmiJle_uUGSBHhrM=",
     harvest: 60,
     growth: 30
   },
