e8b15330f6c99d707e9818af7e156e182790b307
Alexandros Fytilis <alexandrosfytilis@hotmail.com>
2022-10-14 20:06:43 -0400

    Delete buld files

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import CurrentUserProvider from "./general/contexts/CurrenUserProvider";
-const root = createRoot(document.getElementById("root"));
-root.render(React.createElement(React.StrictMode, null,
-    React.createElement(CurrentUserProvider, null,
-        React.createElement(App, null))));
--- client/src/pages/home/hooks/usePlantVegetable.js
+++
@@ -1,9 +0,0 @@
-import { useCallback } from "react";
-import { useUpdateVegetable } from "./useUpdateVegetable";
-export const usePlantVegetable = () => {
-    const updateVegetable = useUpdateVegetable();
-    return useCallback(async (vegetable, date) => {
-        vegetable.datePlanted = date;
-        updateVegetable(vegetable);
-    }, [updateVegetable]);
-};
